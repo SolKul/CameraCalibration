2f2c2a4d2b02e562c85619a63a878f0044e7a9e5
SolKul <tsuneyoshi.gg@gmail.com>
2019-09-20 23:14:50 +0000

    make mymodule

---
+++ 90MyModule/ImageProcessing.py
@@ -0,0 +1,52 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.4'
+#       jupytext_version: 1.2.3
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# +
+import numpy as np
+import cv2
+import os
+import matplotlib.pyplot as plt
+
+def imread(filename, flags=cv2.IMREAD_COLOR, dtype=np.uint8):
+    try:
+        n = np.fromfile(filename, dtype)
+        img = cv2.imdecode(n, flags)
+        return img
+    except Exception as e:
+        print(e)
+        return None
+    
+
+def imwrite(filename, img, params=None):
+    try:
+        ext = os.path.splitext(filename)[1]
+        result, n = cv2.imencode(ext, img, params)
+
+        if result:
+            with open(filename, mode='w+b') as f:
+                n.tofile(f)
+            return True
+        else:
+            return False
+    except Exception as e:
+        print(e)
+        return False
+    
+def show_img(img,figsize=(6,9),isBGR=True):
+    if isBGR:
+        img_cvt=cv2.cvtColor(ar_img,cv2.COLOR_BGR2RGB)
+    else:
+        img_cvt=img
+    plt.figure(figsize=figsize)
+    plt.imshow(img_cvt)
